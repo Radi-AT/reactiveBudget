9abb621cebd6f56d89dc45d1f527ec3097f575f8
Radoslav <56716736+Radi-AT@users.noreply.github.com>
2023-02-02 23:59:46 +0100

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  "env": {
+    "es2021": true,
+    "node": true
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:react/recommended",
+  ],
+  "parserOptions": {
+    "ecmaVersion": "latest",
+    "sourceType": "module",
+    "ecmaFeatures": {
+      "jsx": true
+    },
+  },
+  "plugins": [
+    "react"
+  ],
+  "rules": {
+    // enable additional rules
+    "indent": [2, 2],
+    "semi": 2,
+    "no-trailing-spaces": 2,
+
+    // override configuration set by extending "eslint:recommended"
+    "no-empty": "warn",
+    "no-cond-assign": ["error", "always"],
+  }
+};
---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD-_1kz0N1IFwFjZAvl7XL8Zj-J5nQNRSA",
+  authDomain: "reactivebudget-bd8ce.firebaseapp.com",
+  projectId: "reactivebudget-bd8ce",
+  storageBucket: "reactivebudget-bd8ce.appspot.com",
+  messagingSenderId: "529244588475",
+  appId: "1:529244588475:web:e4a140eea4c971c773dc9e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
